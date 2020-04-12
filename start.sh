c339ce0d46ccf8a7b9b61ff01d27a8a26b07bf9a
Khai Weng Au Yeong <aykhaiweng@gmail.com>
2020-04-13 06:18:57 +0800

    Fixed typo

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-sudo docker run --rm -it -p 7777:7777 -v $(pwd)/mnt/share/:/.local/share/ aykhaiweng:terraria-tmodloader --log-opt max-size=200k
+sudo docker run --rm -it -p 7777:7777 -v $(pwd)/mnt/share/:/.local/share/ aykhaiweng/terraria-tmodloader --log-opt max-size=200k
